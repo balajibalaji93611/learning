c6d5d8ef079b35fa2e9e32b2bc49c7453e9045bb
balajibalaji93611 <71633699+balajibalaji93611@users.noreply.github.com>
2020-09-21 14:07:13 +0530

    my code

---
+++ addition.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+var a int = 5
+var b int = 6
+total := a+b
+fmt.Println("Total is",total)
+}
+
---
+++ addition.py
@@ -0,0 +1,6 @@
+
+a = int(input("enter a number"))
+b =int( input("enter aonther number"))
+total=a+b
+print("total is",total)
+
